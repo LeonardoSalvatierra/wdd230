853711cb7f0c298263911dbe8174921fb01d7264
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-02-11 00:02:57 -0300

    lesson 03

---
+++ lesson03/scripts/getDate.js
@@ -0,0 +1,13 @@
+const newdate = new Date(document.lastModified);
+const year = newdate.getFullYear();
+const month = newdate.getMonth() + 1;
+const day = newdate.getDate();
+const hours = newdate.getHours();
+const minutes = newdate.getMinutes();
+const seconds = newdate.getSeconds();
+
+let currentDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+
+document.querySelector("#time").innerHTML = currentDate;
+document.querySelector("#year").innerHTML = year;
