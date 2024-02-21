84ca5bae9672961776c2300bc8152e85d5615e1b
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-02-21 13:19:33 -0300

    changes

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,6 +1,6 @@
-const input = document.querySelector("favchap");
+const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("list")
+const list = document.querySelector("#list")
 
 button.addEventListener("click", function() {if (input.value != ""){}
 const li = document.createElement('li');
