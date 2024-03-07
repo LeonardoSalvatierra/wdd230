2418f0cd4dc2a2b635078c6181d707f5bf6f1b39
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-03-07 00:31:23 -0300

    changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,18 +1,78 @@
-document.addeventlistener('domcontentloaded', function () {
-    var currentyear = new date().getfullyear();
+function getWeather() {
+    const apiKey = '102faaa19bab5483c8fc8e35ce55dc1e';
 
-    var copyrightyearparagraph = document.queryselector('footer p:first-child');
-    copyrightyearparagraph.textcontent = '®' + currentyear;
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=rosario&appid=${apiKey}`;
 
-    function updatelastmodifieddate() {
-        var lastmodifieddate = document.lastmodified;
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching current weather data:', error);
+            alert('Error fetching current weather data. Please try again.');
+        });
+}
 
-        var lastmodifiedparagraph = document.getelementbyid('lastmodified');
-        lastmodifiedparagraph.textcontent = 'last modification: ' + lastmodifieddate;
+function displayWeather(data) {
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+
+    
+    weatherInfoDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+    } else {
+        const temperature = Math.round(data.main.temp - 273.15); 
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML = `
+            <p>${temperature}°C -</p>
+        `;
+
+        const weatherHtml = `
+            <p> ${description}</p>
+        `;
+
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHtml;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = description;
+
+        showImage();
     }
+}
+function showImage() {
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block'; 
+}
 
-    updatelastmodifieddate();
 
-    var csslink = document.queryselector('link[rel="stylesheet"]');
-    csslink.addeventlistener('load', updatelastmodifieddate);
-});
+const newdate = new Date(document.lastModified);
+const year = newdate.getFullYear();
+const month = newdate.getMonth() + 1;
+const day = newdate.getDate();
+const hours = newdate.getHours();
+const minutes = newdate.getMinutes();
+const seconds = newdate.getSeconds();
+
+let currentDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+
+document.querySelector("#time").innerHTML = currentDate;
+document.querySelector("#year").innerHTML = year;
+
+var numTimes = localStorage.getItem("visits-Hlfma");
+if(numTimes == null) {
+  numTimes = 0;
+} else {
+  numTimes = parseInt(numTimes, 10);
+}
+numTimes++;
+localStorage.setItem("visits-Hlfma", (numTimes).toString(10))
+document.getElementById("visit-times").textContent = numTimes.toString(10);
