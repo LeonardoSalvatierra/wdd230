5ce461f4fd00a8fbf1aa9f001e73abd79ee4df1b
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-03-06 23:12:31 -0300

    changes

--- chamber/scripts/getinfo.js
+++ chamber/scripts/getinfo.js
@@ -111,3 +111,13 @@ function showImage() {
     const weatherIcon = document.getElementById('weather-icon');
     weatherIcon.style.display = 'block'; 
 }
+
+var numTimes = localStorage.getItem("visits-Hlfma");
+if(numTimes == null) {
+  numTimes = 0;
+} else {
+  numTimes = parseInt(numTimes, 10);
+}
+numTimes++;
+localStorage.setItem("visits-Hlfma", (numTimes).toString(10))
+document.getElementById("visit-times").textContent = numTimes.toString(10);
