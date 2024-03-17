fc82a6030db1efed01a009434cf61729f4ec1c41
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-03-17 02:19:11 -0300

    join

--- chamber/scripts/getinfo.js
+++ chamber/scripts/getinfo.js
@@ -121,3 +121,16 @@ if(numTimes == null) {
 numTimes++;
 localStorage.setItem("visits-Hlfma", (numTimes).toString(10))
 document.getElementById("visit-times").textContent = numTimes.toString(10);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener referencia al elemento de entrada oculta
+    const formLoadedInput = document.getElementById('form-loaded');
+
+    // Obtener la fecha y hora actual en milisegundos
+    const currentDateTime = Date.now();
+
+    // Establecer el valor del campo oculto con la fecha y hora actual
+    formLoadedInput.value = currentDateTime;
+});
