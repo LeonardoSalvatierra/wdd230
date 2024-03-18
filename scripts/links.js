11fdf69c5bde5616457d9281f712b48f28696a0a
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-03-17 22:09:12 -0300

    changes

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const baseURL = "https://LeonardoSalvatierra.github.io/wdd230/";
+const linksURL = "https://LeonardoSalvatierra.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+  
+getLinks();
+
+function displayLinks(weeks) {
+    const cards = document.querySelector('#cards');
+    
+    weeks.forEach((week) => {
+        const section = document.createElement('section');
+        const h3 = document.createElement('h3');
+        h3.textContent = `lesson ${week.lesson}`;
+        section.appendChild(h3);
+
+        const ul = document.createElement('ul');
+        week.links.forEach((link) => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = baseURL + link.url;
+            a.textContent = link.title;
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+
+        section.appendChild(ul);
+        cards.appendChild(section);
+    });
+}
