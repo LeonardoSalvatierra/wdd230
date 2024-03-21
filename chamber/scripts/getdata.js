6941bb203ca665cabefd1b75c16e66eafb8ddefa
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2024-03-20 22:59:13 -0300

    changes

--- chamber/scripts/getdata.js
+++ chamber/scripts/getdata.js
@@ -93,3 +93,4 @@ function changeviewlist() {
 getdata();
 
 changeviewlist();
+
--- chamber/scripts/getinfo.js
+++ chamber/scripts/getinfo.js
@@ -135,3 +135,22 @@ document.addEventListener('DOMContentLoaded', function() {
     formLoadedInput.value = currentDateTime;
 });
 
+var banner = document.getElementById("banner");
+
+function closeBanner() {
+    banner.style.display = "none";
+}
+
+function showBannerOnDays() {
+    var today = new Date();
+    var dayOfWeek = today.getDay();
+    
+    if (dayOfWeek >= 1 && dayOfWeek <= 3) {
+        banner.style.display = "block";
+    } else {
+        banner.style.display = "none";
+    }
+}
+
+
+showBannerOnDays();
